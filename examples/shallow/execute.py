79f3123041d4f56aa512670880d5a175f781e56e
Rupert Ford <rupert.ford@stfc.ac.uk>
2016-11-24 17:21:11 +0000

    First running version with shallow and some initial results

--- examples/shallow/execute.py
+++ examples/shallow/execute.py
@@ -19,23 +19,48 @@ def execute(option):
     makefile_option.append({"F90FLAGS":flags})
             
     namelist = create_input(namelist_option, "namelist", template_location="templates")
-    #print namelist
 
     makefile_include = create_input(makefile_option, "Makefile.include", template_location="templates")
-    #print makefile_include
+
+    benchmark_base = "/home/rupert/proj/GOcean/shallow_benchmark"
 
     # save the input files in the appropriate place
+    location = benchmark_base + "/original/namelist"
+    f = open(location, 'w')
+    f.write(namelist)
+    f.flush()
+
+    location = benchmark_base + "/common/Makefile.include"
+    f = open(location, 'w')
+    f.write(makefile_include)
+    f.flush()
 
     # compile shallow if required
+    #import os
+    #os.chdir("/home/rupert/proj/GOcean/shallow_benchmark/original")
+    import subprocess
+    make_process = subprocess.Popen(["make", "clean"], cwd="/home/rupert/proj/GOcean/shallow_benchmark/original",  stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if make_process.wait() != 0:
+        return False, []
+
+    make_process = subprocess.Popen(["make"], cwd="/home/rupert/proj/GOcean/shallow_benchmark/original", stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if make_process.wait() != 0:
+        return False, []
 
     # run shallow
+    make_process = subprocess.Popen(["./shallow_base"], cwd="/home/rupert/proj/GOcean/shallow_benchmark/original", stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if make_process.wait() != 0:
+        return False, []
+    stderr = make_process.stderr.read()
+    stdout = make_process.stdout.read()
 
-    # determine if the run was successful
-    success = True
+    # determine if the results are correct. We will need to look at
+    # the results from stdout but for the moment we assume they are
+    # correct
 
-    results = None
-    if success:
-        # extract the required outputs
-        results = [10]
+    # extract the required outputs
+    for line in stdout.split("\n"):
+        if "Time-stepping" in line:
+            total_time = line.split()[2]
 
-    return success, results
+    return True, total_time
--- examples/shallow/shallow.py
+++ examples/shallow/shallow.py
@@ -8,21 +8,42 @@ from melody.main import Melody
 from execute import execute
 
 # inputs taken from https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
+# more flags and info is here ... https://gcc.gnu.org/onlinedocs/gcc-4.5.3/gcc/i386-and-x86_002d64-Options.html
 INPUTS = [
     Fixed(name="F90", value="gfortran"),
-    Choice(name="F90FLAGS", inputs=["", "-O", "-O1", "-O2", "-O3", "-Ofast"]),
-    Switch(name="Inline limit", on="-inline-limit=1000000"),
-    Switch(name="Stores Out Of Loops", on="-fgcse-sm"),
-    Switch(name="Remove Redundant Loads", on="-fgcse-las"),
-    Switch(name="Loop Nests", on="-floop-nest-optimize"),
-    Choice(name="Vector Cost Model", pre="-fvect-cost-model=",
-           inputs=["unlimited", "dynamic", "cheap"]),
-    Switch(name="Maths Optimisations", on="-funsafe-math-optimizations"),
-    Switch(name="No Nans and infs", on="-ffinite-math-only"),
-    Switch(name="No user traps", on="-fno-trapping-math"),
-    Switch(name="No signed zeros", on="-fno-signed-zeros"),
-    Switch(name="Always unroll", on="-funroll-all-loops"),
-    Choice(name="Problem Size", inputs=["64", "128", "256", "512", "1024"])]
+    Choice(name="F90FLAGS", inputs=["-O2", "-O3", "-Ofast"]),
+    Choice(name="native", inputs=["", "-mtune=native", "-march=native"]),
+    Switch(name="Inline limit", on="-finline-limit=5000"),
+    Fixed(name="Problem Size", value="128")]
+
+# TBD
+# *** try different inline-limit values
+# -fwhole-program needs to be part of compile and fails as we link timer libraries
+#    Switch(name="Single program", on="-fwhole-program"),
+# flto needs to be on both compile and link as do all optimisation options
+
+# ignore lower levels of optimisation
+#    Choice(name="F90FLAGS", inputs=["", "-O", "-O1", "-O2", "-O3", "-Ofast"]),
+
+# start with one problem size to limit the time
+#    Choice(name="Problem Size", inputs=["64", "128", "256"])]
+
+# 512 and 1024 take too long to run
+#    Choice(name="Problem Size", inputs=["64", "128", "256", "512", "1024"])]
+
+# unsupported in my version of gfortran
+#    Choice(name="Vector Cost Model", pre="-fvect-cost-model=",
+#           inputs=["unlimited", "dynamic", "cheap"]),
+
+# ignored for the moment
+#    Switch(name="Stores Out Of Loops", on="-fgcse-sm"),
+#    Switch(name="Remove Redundant Loads", on="-fgcse-las"),
+#    Switch(name="Loop Nests", on="-floop-nest-optimize"),
+#    Switch(name="Maths Optimisations", on="-funsafe-math-optimizations"),
+#    Switch(name="No Nans and infs", on="-ffinite-math-only"),
+#    Switch(name="No user traps", on="-fno-trapping-math"),
+#    Switch(name="No signed zeros", on="-fno-signed-zeros"),
+#    Switch(name="Always unroll", on="-funroll-all-loops"),
 
 MELODY = Melody(inputs=INPUTS, function=execute, method=BruteForce)
 MELODY.search()
