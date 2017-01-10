7a1cb75b048bc637c9dd8b46de82e540b82b6603
Rupert Ford <rupert.ford@stfc.ac.uk>
2017-01-10 13:52:57 +0000

    loop fusion with the gocean api now works for outer loops

--- examples/PSyclone/psyclone.py
+++ examples/PSyclone/psyclone.py
@@ -50,29 +50,31 @@ class ModuleInline(Subsets):
 class GOLoopFuse(object):
     ''' '''
 
-    def _recurse(self, siblings, my_index, output):
+    def _recurse(self, siblings, my_index, options, all_options, invoke):
         from transformations import GOceanLoopFuseTrans, TransformationError
+        my_options = list(options)
         trans = GOceanLoopFuseTrans()
         # siblings includes this loop
         n_siblings = len(siblings)
         index = my_index+1
-        valid = True
-        while index<n_siblings and valid:
+        while index<n_siblings:
             try:
                 trans._validate(siblings[index-1], siblings[index])
-                output.append([index-1, index])
-                print "Fusion for {0} possible".format(output)
-                my_output = []
-                self._recurse(siblings, index+1, output)
+                my_options.append([siblings[index-1], siblings[index]])
+                #print "Fusion for {0} possible".format(my_options)
+                if my_options:
+                    all_options.append({invoke:list(my_options)})
+                self._recurse(siblings, index+1, my_options, all_options, invoke)
             except TransformationError:
                 break
             index += 1
 
     def __init__(self, psy=None, dependent_invokes=False):
         self._name = "Loop Fusion"
+        self._options = []
         self._psy = psy
         invokes = psy.invokes.invoke_list
-        print "there are {0} invokes".format(len(invokes))
+        #print "there are {0} invokes".format(len(invokes))
         if dependent_invokes:
             print ("dependent invokes assumes fusion in one invoke might affect fusion "
                    "in another invoke. This is not yet implemented")
@@ -86,23 +88,9 @@ class GOLoopFuse(object):
                     if loop.loop_type == "outer":
                         siblings = loop.parent.children
                         my_index = siblings.index(loop)
-                        output = []
-                        self._recurse(siblings, my_index, output)
-            exit(1)
-                        
-            #        next = loop.parent.children[my_index+1]
-            #        exit(1)
-            #                    loops.append(loop)
-            #            for index1 in range(len(loops)):
-            #                for index2 in range(index1+1, len(loops)):
-            #                    try:
-            #                        trans._validate(loops[index1], loops[index2])
-            #                        print "Fusion of {0} {1} possible".format(index1, index2)
-            #                    except TransformationError:
-            #                        break
-        exit(1)
-        # get the loop fusion input options from PSyclone
-        self._options = []
+                        option = []
+                        self._recurse(siblings, my_index, option, self._options, invoke)
+            #print self._options
 
     @property
     def options(self):
@@ -122,7 +110,6 @@ _, invoke_info = parse(FILE, api="gocean1.0")
 psy = PSyFactory("gocean1.0").create(invoke_info)
 
 # TBD   ArrayBounds(),
-# TBD   GOLoopFuse(psy=psy),
 INPUTS = [
     GOLoopFuse(psy=psy),
     ModuleInline(psy=psy),
