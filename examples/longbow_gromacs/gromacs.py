3c3a303e85a43d7b05986e72b61f9a9808d30c41
jimboid <james.gebbie@stfc.ac.uk>
2017-02-08 14:21:50 +0000

    Moved Longbow example to recent version.
    Moved Longbow code from branched based on very old version of melody to
    a more recent version.
    
    This example still needs testing with Longbow, but before this can
    happen properly a work-around for the dependent parameters needs to be
    found.

--- examples/longbow_gromacs/gromacs.py
+++ examples/longbow_gromacs/gromacs.py
@@ -36,10 +36,98 @@ jinja2 to set input values, Longbow to run gromacs and some text
 processing to extract the required results. However, we do not yet do
 so.'''
 
+import subprocess
+from random import randint
+
 from melody.inputs import Choice, FloatRange
 from melody.main import Melody
 from melody.search import BruteForce
-from launch import launch
+
+
+def launch(option):
+    '''Launcher method for the gromacs example.
+
+    Set the gromacs input data using the supplied input options, run
+    gromacs and extract and return the required outputs.'''
+    from melody.inputs import create_input
+    inputfile = create_input(option, template_name="input.mdp")
+    results = None
+    success = None
+
+    # Create directory structure for jobs.
+    identifier = "".join(["%s" % randint(0, 9) for _ in range(0, 5)])
+    infile = "gmx" + identifier + ".mdp"
+
+    outfile = open(infile, "w")
+    outfile.write(inputfile)
+    outfile.close()
+
+    try:
+
+        # Run grompp preprocessor.
+        print "handing configuration file '%s' off to grompp..." % infile
+
+        # In gromacs v5+ this should be gmx grompp .....
+        gromppcmdl = ("grompp"
+                      " -f " + infile +
+                      " -p setup.top"
+                      " -c setup.gro"
+                      " -po _" + infile +
+                      " -o run" + identifier + ".tpr")
+
+        handle = subprocess.Popen(gromppcmdl, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE, shell=True)
+        gromppout, grompperr = handle.communicate()
+        setuperrstate = handle.returncode
+
+        # TODO: probably should look at using a log file and log the outputs.
+        if setuperrstate != 0:
+
+            raise OSError
+
+        print "grompp ran successfully."
+
+        # Fire off run with Longbow.
+        print "handing simulation off to Longbow for execution..."
+
+        # In gromacs v5+ this should be gmx mdrun ....
+        longbowcmdl = ("longbow --verbose mdrun -deffnm run" + identifier)
+
+        # TODO: realtime status updates by capturing stdout in real time is
+        # better.
+        #handle = subprocess.Popen(longbowcmdl, stdout=subprocess.PIPE,
+        #                          stderr=subprocess.PIPE, shell=True)
+        #longbowout, longbowerr = handle.communicate()
+        #runerrstate = handle.returncode
+
+        # Determine if the run was successful
+        #if runerrstate != 0:
+
+        #    raise OSError
+
+        #else:
+
+            #print "job ran successfully and all results staged back."
+            #success = True
+
+    except OSError:
+
+        success = False
+
+    # Extract the required outputs.
+    if success:
+
+        results = {"id": {"value": identifier}}
+        resfile = open("run" + identifier + ".log", "r").readlines()
+
+        for line in resfile:
+
+            if "Performance:" in line:
+
+                results["nspd"] = {"value": line.split()[1], "units": "ns/day"}
+                break
+
+    return success, results
 
 INPUTS = [Choice(name="tcoupl", inputs=["Berendsen", "Nose-Hoover"]),
           FloatRange(name="rcoulomb", low=1.1, high=1.6, step=0.1)]
