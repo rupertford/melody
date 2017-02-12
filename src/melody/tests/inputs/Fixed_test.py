24b4262a561ed3921a42bd80cdefa7ad8c16a292
Rupert Ford <rupert.ford@stfc.ac.uk>
2017-02-12 02:02:42 +0000

    issue #8. First set of tests for inputs.py

---
+++ src/melody/tests/inputs/Fixed_test.py
@@ -0,0 +1,58 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2017, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author R. Ford STFC Daresbury Lab.
+#
+'''pytest tests for the Fixed class in the inputs.py melody file'''
+
+import pytest
+from melody.inputs import Fixed
+
+def test_fixed_class_vanilla():
+    '''check that initial values are set to None if they are not
+    provided'''
+    fixed = Fixed()
+    assert len(fixed.options) == 1
+    assert fixed.options[0] == None
+    assert fixed.name == None
+
+def test_fixed_class_value():
+    '''check that the Fixed class returns the specified value'''
+    test_value = "test"
+    fixed = Fixed(value=test_value)
+    assert len(fixed.options) == 1
+    assert fixed.options[0] == test_value
+
+def test_fixed_class_name():
+    '''check that the fixed class returns the specified name'''
+    test_name = "wife"
+    fixed = Fixed(name=test_name)
+    assert fixed.name == test_name
