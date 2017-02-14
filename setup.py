7f17551c0236e818c4aab83ab50a361757bc1b2d
Rupert Ford <rupert.ford@stfc.ac.uk>
2017-02-14 00:11:12 +0000

    issue #16. Basic manifest.in and setup.py files for pip.

---
+++ setup.py
@@ -0,0 +1,83 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2017, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author R. Ford STFC Daresbury Lab.
+#
+"""Setup script. Used by easy_install and pip."""
+
+from setuptools import setup, find_packages
+
+PACKAGES = find_packages(where="src")
+
+NAME = 'melody'
+AUTHOR = 'Rupert Ford'
+AUTHOR_EMAIL = 'rupert.ford@stfc.ac.uk'
+URL = 'https://github.com/rupertford/melody'
+DOWNLOAD_URL = 'https://github.com/rupertford/melody'
+DESCRIPTION = 'melody'
+LONG_DESCRIPTION = '''melody: lightweight python parameter search software for performance analysis and tuning'''
+LICENSE='OSI Approved :: BSD 3-Clause License'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development',
+    'Topic :: Utilities',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS']
+
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+ISRELEASED = not True
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+if __name__ == '__main__':
+
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=(AUTHOR_EMAIL),
+        license=LICENSE,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        install_requires=['fparser'])
+
